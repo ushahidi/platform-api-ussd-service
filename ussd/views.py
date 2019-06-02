44a6b0d65cc1fa41ea25aad24395933e5e2b52ff
Timothy Olaleke <timothyolaleke@gmail.com>
2019-06-02 13:21:30 +0100

    Get Access Token

--- ussd/views.py
+++ ussd/views.py
@@ -1,8 +1,25 @@
 import os
+import json
+import requests
+import urllib.parse
 from django.shortcuts import render
 from django.http import HttpResponse
 
 PLATFORM_URL = os.environ.get('PLATFORM_URL', 'PLATFORM_URL NOT AVAILABLE')
+EMAIL = os.environ.get('PLATFORM_EMAIL', '')
+PASSWORD = os.environ.get('PLATFORM_PASSWORD', '')
+AUTH_URL = urllib.parse.urljoin(PLATFORM_URL, '/oauth/token/')
+ACCESS_TOKEN = None
+
+def get_access_token():
+    payload = {
+        'username': EMAIL,
+        'password': PASSWORD,
+        'client_id': 'ushahidiui',
+        'client_secret': '35e7f0bca957836d05ca0492211b0ac707671261',
+        'scope': '*'
+    }
+    return requests.request('POST', AUTH_URL, data=json.dumps(payload))   
 
 def index(request):
-    return HttpResponse(PLATFORM_URL, content_type="text/plain")
+    return HttpResponse(get_access_token(), content_type='text/plain')
