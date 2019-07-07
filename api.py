846caa7a040982a626c290ba9016c759e7e5af51
Timothy Olaleke <timothyolaleke@gmail.com>
2019-07-07 19:59:33 +0100

    Implementing Posting to Ushahidi

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 import os
 import json
 import requests
+from utils import *
+from datetime import datetime
 
 PLATFORM_API = os.environ.get('PLATFORM_API', '')
 EMAIL = os.environ.get('PLATFORM_EMAIL', '')
@@ -37,5 +39,49 @@ def form_attributes(id):
     response = requests.request("GET", url, headers=forms_header, params=querystring).text
     response = json.loads(str(response))['results']
     for field in response:
-        fields.append({"label": field['label'].upper(), "key": field['key'], "type": field['input']})
+        fields.append({"label": field['label'].upper(), "key": field['key'], "type": field['input'], "options": field['options']})
     return fields
+
+# Method to post USSD reponses to Ushahidi Dashboard - accepts Survey ID, Fields Array and User Inputs from USSD
+def post_ussd_responses(id, fields, ussd_responses):
+    # Build post payload
+    payload = {
+        'title': '',
+        'content': '',
+        'locale': 'en_US',
+        'values': {},
+        'post_date': datetime.now().strftime('%Y-%m-%d %H:%M:%SZ'),
+        'form': {
+            'id': id
+        }
+    }
+    for i, field in enumerate(fields):
+        print(ussd_responses(i))
+        # Set Title & Content values
+        if i==0:
+            payload['title'] = ussd_responses[i]
+        elif i==1:
+            payload['content'] = ussd_responses[i]
+        else:
+            # Loop through fields, get the key and corresponding responses
+            key = field['key']
+            payload['values'][key] = ussd_responses(i).split(',') # Convert response into Arrays
+            # Modify Special Fields - Override Response Values
+            # Set Location Coordinates
+            if (fields[i]['type'] == 'location'):
+                coordinates = get_location_coordinates(ussd_responses(i)) # Geocode User Location
+                payload['values'][key] = {
+                    'lat': coordinates[0],
+                    'lon': coordinates[1]
+                }
+            # Set Datetime values
+            if (fields[i]['type'] == 'date') or (fields[i]['type'] == 'datetime'):
+                datetime_string = ussd_responses(i)
+                datetime_object =  datetime.fromisoformat(datetime_string).strftime('%Y-%m-%d %H:%M:%SZ')
+                payload['values'][key] = datetime_object
+            # Set Actual Values from Multichoices
+    # Make Post request to Submit data
+    payload = json.dumps(payload)
+    post_url = "{}/api/v3/posts".format(PLATFORM_API)
+    requests.request("POST", post_url, data=payload, headers=forms_header)
+    print(payload)
